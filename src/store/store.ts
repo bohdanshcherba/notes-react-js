e83bbb54f487d6096e6d4c2e1fd2eb95ee843f8c
Bohdan Shcherba <73034554+bohdanshcherba@users.noreply.github.com>
2023-07-29 17:56:00 +0300

    config redux

---
+++ src/store/store.ts
@@ -0,0 +1,21 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
+
+export const store = configureStore({
+    reducer: {
+
+    },
+})
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export default store
+
+export type AsyncThunkConfig = {
+    state: RootState;
+    dispatch: AppDispatch;
+    extra: {};
+};
